ce3363cbec25bb4be4994d3ec0c17732dfe0b527
amrakun <amraa.888@gmail.com>
2023-07-20 23:54:13 +0900

    Cloned first 1Hour video

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { FunctionComponent, useEffect } from "react"
+
+interface PageProps {}
+
+const Page: FunctionComponent<PageProps> = () => {
+  const onOpen = useStoreModal(state => state.onOpen)
+  const isOpen = useStoreModal(state => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) onOpen()
+  }, [isOpen, onOpen])
+
+  return <div>rootPage</div>
+}
+
+export default Page
---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { FunctionComponent, useEffect, useState } from "react"
+import { StoreModal } from "@/components/modals/store-modal"
+
+interface ModalProviderProps {}
+
+const ModalProvider: FunctionComponent<ModalProviderProps> = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  )
+}
+
+export default ModalProvider
