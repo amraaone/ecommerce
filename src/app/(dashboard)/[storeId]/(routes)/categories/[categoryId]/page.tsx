cda12d8c110b6d13a977d4852a0c40faa2bcbc70
amrakun <amraa.888@gmail.com>
2023-09-18 23:36:05 +0900

    category menu added

---
+++ src/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,34 @@
+import prismadb from "@/lib/prismadb"
+import { FunctionComponent } from "react"
+import CategoryForm from "./components/category-form"
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string
+    storeId: string
+  }
+}
+
+const CategoryPage: FunctionComponent<CategoryPageProps> = async ({
+  params: { categoryId, storeId },
+}) => {
+  const category = await prismadb.category.findUnique({
+    where: { id: categoryId },
+  })
+
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: storeId,
+    },
+  })
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm billboards={billboards} initialData={category} />
+      </div>
+    </div>
+  )
+}
+
+export default CategoryPage
---
+++ src/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,42 @@
+import { FunctionComponent } from "react"
+import { format } from "date-fns"
+import prismadb from "@/lib/prismadb"
+import CategoryClient from "./components/client"
+import { CategoryColumn } from "./components/columns"
+
+interface BillboardsProps {
+  params: { storeId: string }
+}
+
+const CategoriesPage: FunctionComponent<BillboardsProps> = async ({
+  params,
+}) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  })
+
+  const formattedCategories: CategoryColumn[] = categories.map(item => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient data={formattedCategories} />
+      </div>
+    </div>
+  )
+}
+
+export default CategoriesPage
